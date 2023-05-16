b258ccc64fac4c7883f0ee7795e923dca9078aa3
Bo Lu <lubobill1990@163.com>
2023-05-16 10:55:31 +0800

    add grayscale effect

--- videoapp/src/App.tsx
+++ videoapp/src/App.tsx
@@ -1,18 +1,26 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import './App.css'
 import { app, video } from "@microsoft/teams-js";
 import { WebGL2Grayscale } from './webgl2';
 
 function App() {
-  const [count, setCount] = useState(0)
   const effectIdRef = useRef<string | undefined>(undefined);
   const initializeVideoApp = useCallback(() => {
+    let grayscale: WebGL2Grayscale | null = null;
     video.registerForVideoEffect((effectId: string | undefined) => {
       console.log(`New effect selected ${effectId}`);
       effectIdRef.current = effectId;
+      if (effectId === '00000000-0000-0000-0001-000000000000') {
+        if (grayscale === null) {
+          grayscale = new WebGL2Grayscale();
+          grayscale.setup();
+        }
+      } else {
+        grayscale = null;
+      }
       return Promise.resolve();
     });
-    video.mediaStream.registerForVideoFrame((frame: any) => {
+    video.mediaStream.registerForVideoFrame(async (frame: any) => {
       const effectId = effectIdRef.current;
       const videoFrame = frame.videoFrame as VideoFrame;
       if (effectId?.indexOf("00000000-0000-0000-0000-0000") === 0) {
@@ -24,15 +32,24 @@ function App() {
         });
       }
       if (effectId === '00000000-0000-0000-0001-000000000000') {
-
+        if (grayscale === null) {
+          throw 'Grayscale effect is not initialized';
+        }
+        const imageBitmap = await createImageBitmap(videoFrame);
+        grayscale.draw(imageBitmap);
+        const grayscaled = new VideoFrame(grayscale.getCanvas(), {
+          timestamp: videoFrame.timestamp,
+          displayHeight: videoFrame.displayHeight,
+          displayWidth: videoFrame.displayWidth,
+        });
+        videoFrame.close();
+        return grayscaled;
       }
       return Promise.resolve(videoFrame);
     })
   }, [effectIdRef])
 
-
   useEffect(() => {
-    return;
     app.initialize([
       "https://microsoft.github.io"
     ]).then(() => {
@@ -40,48 +57,9 @@ function App() {
     });
   }, [initializeVideoApp]);
 
-  useEffect(() => {
-    if (count === 0) {
-      return;
-    }
-    navigator.mediaDevices.getDisplayMedia({}).then((stream) => {
-      // turn to VideoFrame using TransformStream
-      const videoTrack = stream.getVideoTracks()[0];
-      // initialize a MediaStreamTrackProcessor from the videoTrack
-      const processor = new MediaStreamTrackProcessor({ track: videoTrack });
-      // initialize a MediaStreamTrackGenerator
-      const generator = new MediaStreamTrackGenerator({ kind: 'video' });
-
-      const grayscale = new WebGL2Grayscale();
-      grayscale.setup();
-      const transformStream = new TransformStream({
-        transform: async (chunk: VideoFrame, controller) => {
-          const imageBitmap = await createImageBitmap(chunk);
-          grayscale.draw(imageBitmap);
-          const grayscaled = new VideoFrame(grayscale.getCanvas(), {
-            timestamp: chunk.timestamp,
-            displayHeight: chunk.displayHeight,
-            displayWidth: chunk.displayWidth,
-          });
-          chunk.close();
-          controller.enqueue(grayscaled);
-        }
-      });
-      const readableStream = processor.readable;
-      const writableStream = generator.writable;
-      readableStream.pipeThrough(transformStream).pipeTo(writableStream);
-      const outputMediaStream = new MediaStream([generator]);
-      videoRef.current!.srcObject = outputMediaStream;
-    })
-  }, [count === 0]);
-
-  const videoRef = useRef<HTMLVideoElement>(null);
   return <>
     <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      <video autoPlay ref={videoRef} style={{ width: "100%" }}></video>
+      <h1>This is a Microsoft Teams sample video app</h1>
     </div>
   </>
 }
--- videoapp/src/webgl2.ts
+++ videoapp/src/webgl2.ts
@@ -123,8 +123,8 @@ export class WebGL2Grayscale {
         this.gl.viewport(0, 0, imageBitmap.width, imageBitmap.height);
         const texture = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageBitmap);
         gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageBitmap);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
