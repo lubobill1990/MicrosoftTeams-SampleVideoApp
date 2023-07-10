7777b76982da6674f06718f2d357df62821692e9
Bo Lu <lubobill1990@163.com>
2023-07-10 18:43:07 +0800

    change videoBufferHandler for T1

--- videoapp/src/App.tsx
+++ videoapp/src/App.tsx
@@ -1,8 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useCallback, useEffect, useRef } from 'react'
-import './App.css'
+import { useCallback, useEffect, useRef } from "react";
+import "./App.css";
 import { app, video } from "@microsoft/teams-js";
-import { WebGL2Grayscale } from './webgl2';
+import { WebGL2Grayscale } from "./webgl2";
+
+function getLatencyFromEffectId(effectId: string): number {
+  if (effectId.indexOf("00000000-0000-0000-0000-0000") === 0) {
+    return parseInt(effectId.split("-")[4]);
+  }
+  return 0;
+}
 
 function App() {
   const canvasWrapRef = useRef<HTMLDivElement>(null);
@@ -10,20 +17,20 @@ function App() {
   const effectIdRef = useRef<string | undefined>(undefined);
   const initializeVideoApp = useCallback(() => {
     let grayscale: WebGL2Grayscale | null = null;
-    let currentSize = '';
-    let ctx2d = originCanvasRef.current?.getContext('2d');
+    let currentSize = "";
+    let ctx2d = originCanvasRef.current?.getContext("2d");
     video.registerForVideoEffect((effectId: string | undefined) => {
       console.log(`New effect selected ${effectId}`);
-      if (effectId === '00000000-0000-0001-0000-000000000000') {
+      if (effectId === "00000000-0000-0001-0000-000000000000") {
         return Promise.reject();
       }
       effectIdRef.current = effectId;
-      if (effectId === '00000000-0000-0000-0001-000000000000') {
+      if (effectId === "00000000-0000-0000-0001-000000000000") {
         if (grayscale === null) {
           grayscale = new WebGL2Grayscale();
           grayscale.setup();
           canvasWrapRef.current?.appendChild(grayscale.getCanvas());
-          console.log('Append canvas');
+          console.log("Append canvas");
         }
       } else {
         grayscale = null;
@@ -31,39 +38,53 @@ function App() {
       return Promise.resolve();
     });
 
-    const videoFrameHandler = async (frame: video.VideoFrameData): Promise<VideoFrame> => {
+    const videoFrameHandler = async (
+      frame: video.VideoFrameData
+    ): Promise<VideoFrame> => {
       const effectId = effectIdRef.current;
       const videoFrame = frame.videoFrame as VideoFrame;
-      if (effectId === '00000000-0000-0000-0000-000000000000') {
+      if (!effectId || effectId === "00000000-0000-0000-0000-000000000000") {
         return videoFrame;
       }
-      if (currentSize !== `${videoFrame.displayWidth}x${videoFrame.displayHeight}`) {
+      if (
+        currentSize !== `${videoFrame.displayWidth}x${videoFrame.displayHeight}`
+      ) {
         const newSize = `${videoFrame.displayWidth}x${videoFrame.displayHeight}`;
         console.log(`Input size changed from ${currentSize} to ${newSize}`);
         currentSize = newSize;
       }
-      
-      const imageBitmap = await createImageBitmap(videoFrame);
-      // Draw original video frame
-      if (originCanvasRef.current && videoFrame.displayWidth !== originCanvasRef.current.width) {
-        originCanvasRef.current.width = videoFrame.displayWidth;
-        originCanvasRef.current.height = videoFrame.displayHeight;
-        ctx2d = originCanvasRef.current?.getContext('2d');
-      }
-      ctx2d?.drawImage(imageBitmap, 0, 0, videoFrame.displayWidth, videoFrame.displayHeight);
-      
-      if (effectId?.indexOf("00000000-0000-0000-0000-0000") === 0) {
-        const latency = parseInt(effectId.split('-')[4]);
+
+      const latency = getLatencyFromEffectId(effectId);
+      if (latency > 0) {
         return new Promise((resolve) => {
           setTimeout(() => {
             resolve(videoFrame);
           }, latency);
         });
       }
-      if (effectId === '00000000-0000-0000-0001-000000000000') {
+      
+      if (effectId === "00000000-0000-0000-0001-000000000000") {
         if (grayscale === null) {
-          throw 'Grayscale effect is not initialized';
+          throw "Grayscale effect is not initialized";
         }
+
+        const imageBitmap = await createImageBitmap(videoFrame);
+        // Draw original video frame
+        if (
+          originCanvasRef.current &&
+          videoFrame.displayWidth !== originCanvasRef.current.width
+        ) {
+          originCanvasRef.current.width = videoFrame.displayWidth;
+          originCanvasRef.current.height = videoFrame.displayHeight;
+          ctx2d = originCanvasRef.current?.getContext("2d");
+        }
+        ctx2d?.drawImage(
+          imageBitmap,
+          0,
+          0,
+          videoFrame.displayWidth,
+          videoFrame.displayHeight
+        );
         grayscale.draw(imageBitmap);
         const grayscaled = new VideoFrame(grayscale.getCanvas(), {
           timestamp: videoFrame.timestamp,
@@ -74,14 +95,32 @@ function App() {
         return grayscaled;
       }
       return videoFrame;
-    }
-    
+    };
+
     video.registerForVideoFrame({
       videoFrameHandler,
       /**
-          * Callback function to process the video frames shared by the host.
-          */
-      videoBufferHandler: async (videoBufferData: video.VideoBufferData, notifyVideoFrameProcessed, _notifyError) => {
+       * Callback function to process the video frames shared by the host.
+       */
+      videoBufferHandler: async (
+        videoBufferData: video.VideoBufferData,
+        notifyVideoFrameProcessed,
+        _notifyError
+      ) => {
+        const effectId = effectIdRef.current;
+        if (!effectId || effectId === "00000000-0000-0000-0000-000000000000") {
+           notifyVideoFrameProcessed();
+           return;
+        }
+
+        const latency = getLatencyFromEffectId(effectId);
+        if (latency > 0) {
+            setTimeout(() => {
+              notifyVideoFrameProcessed();
+            }, latency);
+            return;
+        } 
+
         const frame = new VideoFrame(videoBufferData.videoFrameBuffer, {
           format: "NV12",
           codedWidth: videoBufferData.width,
@@ -89,7 +128,7 @@ function App() {
           timestamp: Date.now(),
         });
         const outputFrame = await videoFrameHandler({
-          videoFrame: frame
+          videoFrame: frame,
         });
         const bitmap = await createImageBitmap(outputFrame);
         console.log(bitmap.width);
@@ -97,31 +136,31 @@ function App() {
         notifyVideoFrameProcessed();
       },
       /**
-          * Video frame configuration supplied to the host to customize the generated video frame parameters, like format
-          */
+       * Video frame configuration supplied to the host to customize the generated video frame parameters, like format
+       */
       config: {
-        format: video.VideoFrameFormat.NV12
-      }
+        format: video.VideoFrameFormat.NV12,
+      },
     });
-  }, [effectIdRef])
+  }, [effectIdRef]);
 
   useEffect(() => {
-    app.initialize([
-      "https://microsoft.github.io"
-    ]).then(() => {
-      initializeVideoApp()
+    app.initialize(["https://microsoft.github.io"]).then(() => {
+      initializeVideoApp();
     });
   }, [initializeVideoApp]);
 
-  return <>
-    <div className="card">
-      <h1>This is a Microsoft Teams sample video app</h1>
-      <h3>Original video</h3>
-      <canvas ref={originCanvasRef} style={{ width: '100%' }}></canvas>
-      <h3>Processed video</h3>
-      <div ref={canvasWrapRef} style={{ width: '100%' }}></div>
-    </div>
-  </>
+  return (
+    <>
+      <div className="card">
+        <h1>This is a Microsoft Teams sample video app</h1>
+        <h3>Original video</h3>
+        <canvas ref={originCanvasRef} style={{ width: "100%" }}></canvas>
+        <h3>Processed video</h3>
+        <div ref={canvasWrapRef} style={{ width: "100%" }}></div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
